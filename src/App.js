68d6cea7b2ea7b2e8cbc9142ea1b37b7a939bfba
Diogo <diogo.peralta.goncalves@gmail.com>
2023-07-15 09:43:33 +0100

    pls

--- src/App.js
+++ src/App.js
@@ -23,23 +23,10 @@ function App() {
   useEffect(() => {
     const updateDB = async () => {
       if (logged === false) return console.log('nothing to send');
-      console.log(`Bearer ${bToken}`)
+      
 
-      const response = fetch("https://axiomatic-marshy-galley.glitch.me/verify", {
-          method: "POST",
-          headers: {
-             'Authorization': 'Bearer ' + bToken
-            }})
-            console.log(response.status)
-      if ( response.status != 200 ) {
-        const refresh = fetch("https://axiomatic-marshy-galley.glitch.me/refresh", {
-          method: "GET",
-          credentials: 'include'
-        })
-        
-        console.log(refresh.status)
+      verifyToken();
 
-      } else if (response.status === 200) {
 
       if (reqType === ''){
         return console.log('nothing to send')
@@ -84,19 +71,30 @@ function App() {
       else  {
         console.log('Not sure why it reached this log..')
       }
-    }
+    
    }
     updateDB()
   }, [items])
 
   useEffect(() => {
+  /*   fetch('https://axiomatic-marshy-galley.glitch.me/auth', {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+         },
+          body: JSON.stringify(
+            {
+              "user": `${user}`,
+            "pwd": `${pwd}`
+          }
+        )       
+        }) */
     if (logged === false) {   
       navigate('/login')
    }
   }, [])
 
   useEffect(() => {
-          
        const fetchItems = async () => {
        
         if (logged === true) {
@@ -125,17 +123,41 @@ function App() {
     fetchItems();
   }, [load]);
  
-const addItem = (item) => {    
+const addItem = async (item) => {    
   const id = items.length ? items[items.length - 1].id + 1 : 1;
   const myNewItem = {
     "name": item,
     "id": id,
     "checked": false
   } 
+  const check = await verifyToken();
+  if (check === false) return console.log('logged out')
+  console.log('this should not be logged');
+  
   setNewItem(myNewItem)
   const listItems = [...items, myNewItem];
   setReqType(myNewItem)
   setItems(listItems);
+}
+
+const verifyToken = async () => {
+  const response = await fetch("https://axiomatic-marshy-galley.glitch.me/verify", {
+    method: "POST",
+    headers: {
+       'Authorization': 'Bearer ' + bToken
+      }})
+ 
+if ( response.status !== 200 ) {
+  const refresh = await fetch("https://axiomatic-marshy-galley.glitch.me/refresh", {
+    method: "GET",
+    credentials: 'include'
+  })
+  if( refresh.status === 200) return console.log('proceed');
+  else {setLogged(false)
+    navigate('/login')
+    return false;
+  }
+} else return console.log('proceed:)');
 }
 
 const handleCheck = (id) => {
